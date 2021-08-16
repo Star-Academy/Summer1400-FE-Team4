78e29c269def2a6ddf4715f9d8d909abd20be0df
= <saedi.behnam.336@gmail.com>
2021-08-16 10:30:46 +0430

    Add toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,43 @@ import { AsideComponent } from './aside/aside.component';
 import { SongListComponent } from './song-list/song-list.component';
 import { LandingComponent } from './landing/landing.component';
 import { ShelfComponent } from './shelf/shelf.component';
-//import { SongComponent } from './song/song.component';
 import { RouterModule } from '@angular/router';
 import { SongService } from './common/song.service';
-//import { BannerComponent } from './banner/banner.component';
+import { BannerComponent } from './banner/banner.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './common';
 import { FormsModule } from '@angular/forms';
-//import {UserModule} from "./user/user.module";
+import { UserModule } from './user/user.module';
 import { SearchTermService } from './song-list/search-term.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
         AsideComponent,
+        BannerComponent,
         SongListComponent,
         LandingComponent,
         ShelfComponent,
+        ArtistComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        ToastrModule.forRoot({
+            positionClass: 'toast-bottom-right',
+            preventDuplicates: true,
+            resetTimeoutOnDuplicate: true,
+            disableTimeOut: true,
+        }),
+        UserModule,
+    ],
     providers: [ApiService, SongService, SearchTermService],
     bootstrap: [AppComponent],
 })
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { SongService } from '../common';
 import { ShelfCard } from '../shelf/shelf.component';
 
@@ -13,42 +14,49 @@ export class LandingComponent implements OnInit {
     singerCards: ShelfCard[] = [];
     newAlbumCards: ShelfCard[] = [];
 
-    constructor(private songService: SongService, private route: Router) {}
+    constructor(
+        private songService: SongService,
+        private toastr: ToastrService,
+        private route: Router
+    ) {}
 
     ngOnInit(): void {
         this.songService.getSongs().subscribe(
-            (songs) =>
-                (this.albumCards = songs.map((song) => ({
+            (songs) => {
+                this.albumCards = songs.map((song) => ({
                     link: ['album', song.id],
                     title: song.name,
                     image: song.cover,
-                }))),
-            (error) => alert(error.message) // Use toaster instead
+                }));
+            },
+            (error) => this.toastr.error(error.message)
         );
 
         this.songService.getSongs('name', undefined, 20, 2).subscribe(
-            (songs) =>
-                (this.singerCards = songs
+            (songs) => {
+                this.singerCards = songs
                     // remove duplicates
                     .filter((value, index, self) => {
                         return self.findIndex((other) => other.artist === value.artist) === index;
                     })
                     .map((song) => ({
-                        link: ['singer', song.artist],
+                        link: ['artist', song.artist],
                         title: song.artist,
                         image: song.cover,
-                    }))),
-            (error) => alert(error.message) // Use toaster instead
+                    }));
+            },
+            (error) => this.toastr.error(error.message)
         );
 
         this.songService.getSongs(undefined, undefined, 10, 2).subscribe(
-            (songs) =>
-                (this.newAlbumCards = songs.map((song) => ({
+            (songs) => {
+                this.newAlbumCards = songs.map((song) => ({
                     link: ['album', song.id],
                     title: song.name,
                     image: song.cover,
-                }))),
-            (error) => alert(error.message) // Use toaster instead
+                }));
+            },
+            (error) => this.toastr.error(error.message)
         );
     }
 }
