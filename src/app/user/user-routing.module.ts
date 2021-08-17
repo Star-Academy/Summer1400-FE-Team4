ce93ed576a3db4ca3c611b17cda425c776c97b37
hadi <hmp123475@gmail.com>
2021-08-17 17:57:33 +0430

    create profile page

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'signUp', component: SignUpComponent },
+    { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,9 +5,16 @@ import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
-    declarations: [LoginComponent, UserComponent, SignUpComponent, SignUpComponent],
+    declarations: [
+        LoginComponent,
+        UserComponent,
+        SignUpComponent,
+        SignUpComponent,
+        ProfileComponent,
+    ],
     imports: [CommonModule, UserRoutingModule, FormsModule],
 })
 export class UserModule {}
