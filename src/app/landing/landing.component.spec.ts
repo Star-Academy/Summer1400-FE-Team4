4c5dfc8320c4c27f0f13a0cab20084c892047d58
= <saedi.behnam.336@gmail.com>
2021-08-25 17:48:27 +0430

    Add LandingComponent tests

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -1,28 +1,62 @@
-import { SharedCommonService, SongService } from '../common';
+import { ApiError, SharedCommonService, Song, SongService } from '../common';
 import { ToastrService } from 'ngx-toastr';
 import { LandingComponent } from './landing.component';
-import { of, Subject } from 'rxjs';
+import { of, Subject, throwError } from 'rxjs';
 
 describe('landingComponent', () => {
-    let mockSharedCommon: SharedCommonService;
-    let mockSongService: SongService;
+    let sharedCommon: SharedCommonService;
+    let mockSongService: jasmine.SpyObj<SongService>;
     let mockToastr: ToastrService;
-    let landingComponent: LandingComponent;
+    let component: LandingComponent;
+
+    let dummySongs: Song[];
 
     beforeEach(() => {
-        mockSongService = jasmine.createSpyObj('mockSongService', { getSongs: of() });
-        mockToastr = jasmine.createSpyObj('mockToastr', ['success', 'error']);
-        mockSharedCommon = jasmine.createSpyObj('mockSharedCommon', [], {
-            topBarDark: new Subject<boolean>(),
-        });
-        landingComponent = new LandingComponent(mockSharedCommon, mockSongService, mockToastr);
+        dummySongs = [
+            { id: 5, artist: '123', name: 'fünf' },
+            { id: 6, artist: '12', name: 'sechs' },
+            { id: 7, artist: '12', name: 'sieben' },
+            { id: 8, artist: '123', name: 'acht' },
+        ] as Song[];
+
+        sharedCommon = new SharedCommonService();
+        mockSongService = jasmine.createSpyObj<SongService>({ getSongs: of() });
+        mockToastr = jasmine.createSpyObj<ToastrService>(['error']);
+
+        component = new LandingComponent(sharedCommon, mockSongService, mockToastr);
     });
+
     describe('ngOnInit', () => {
-        describe('getSongs', () => {
-            it('should call getSongs()', () => {
-                landingComponent.ngOnInit();
-                expect(mockSongService.getSongs).toHaveBeenCalledTimes(3);
-            });
+        it('should update topBarDark', () => {
+            spyOn(sharedCommon.topBarDark, 'next').and.callThrough();
+            component.ngOnInit();
+
+            expect(sharedCommon.topBarDark.next).toHaveBeenCalled();
+        });
+
+        it('should update songs on success', () => {
+            mockSongService.getSongs.and.returnValue(of(dummySongs));
+            component.ngOnInit();
+
+            expect(component.albumCards.length).toBe(4);
+            expect(component.singerCards.length).toBe(2);
+            expect(component.newAlbumCards.length).toBe(4);
+
+            expect(mockSongService.getSongs).toHaveBeenCalledTimes(3);
+            expect(mockToastr.error).not.toHaveBeenCalled();
+        });
+
+        it('should handle error on failure', () => {
+            mockSongService.getSongs.and.returnValue(throwError(new ApiError('argh!')));
+            component.ngOnInit();
+
+            expect(component.albumCards.length).toBe(0);
+            expect(component.singerCards.length).toBe(0);
+            expect(component.newAlbumCards.length).toBe(0);
+
+            expect(mockSongService.getSongs).toHaveBeenCalledTimes(3);
+            expect(mockToastr.error).toHaveBeenCalledTimes(3);
+            expect(mockToastr.error).toHaveBeenCalledWith('argh!');
         });
     });
 });
